a4e5cae5f92df74fc9ef5215c0762cba6264c798
KaiHao <okh8609@gmail.com>
2019-10-20 12:28:01 +0800

    Auto Formatting

--- Maze/OpenGLWidget.cpp
+++ Maze/OpenGLWidget.cpp
@@ -6,33 +6,33 @@
 
 OpenGLWidget::OpenGLWidget(QWidget *parent) : QGLWidget(parent)
 {
-	
+
 	top_z = 1.5f;
 	but_z = -1;
 
 	QDir dir("Pic");
-	if(dir.exists())
+	if (dir.exists())
 		pic_path = "Pic/";
 	else
 		pic_path = "../x64/Release/Pic/";
 }
 void OpenGLWidget::initializeGL()
 {
-	glClearColor(0,0,0,1);
+	glClearColor(0, 0, 0, 1);
 	glEnable(GL_TEXTURE_2D);
-	loadTexture2D(pic_path + "grass.png",grass_ID);
-	loadTexture2D(pic_path + "sky.png",sky_ID);
+	loadTexture2D(pic_path + "grass.png", grass_ID);
+	loadTexture2D(pic_path + "sky.png", sky_ID);
 }
 void OpenGLWidget::paintGL()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	if(MazeWidget::maze!=NULL)
+	if (MazeWidget::maze != NULL)
 	{
 		//View 1
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
-		glViewport(0 , 0 , MazeWidget::w/2 , MazeWidget::h);
-		glOrtho (-0.1, MazeWidget::maze->max_xp +0.1, -0.1 , MazeWidget::maze->max_yp +0.1, 0 , 10);
+		glViewport(0, 0, MazeWidget::w / 2, MazeWidget::h);
+		glOrtho(-0.1, MazeWidget::maze->max_xp + 0.1, -0.1, MazeWidget::maze->max_yp + 0.1, 0, 10);
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
 		Mini_Map();
@@ -40,7 +40,7 @@ void OpenGLWidget::paintGL()
 		//View 2
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
-		glViewport(MazeWidget::w/2,0, MazeWidget::w/2, MazeWidget::h);
+		glViewport(MazeWidget::w / 2, 0, MazeWidget::w / 2, MazeWidget::h);
 		/*gluPerspective 定義透視
 		//視野大小, nearplane, farplane, distance
 		//Note: You shouldn't use this function to get view matrix, otherwise you will get 0.
@@ -65,44 +65,44 @@ void OpenGLWidget::paintGL()
 		Map_3D();
 	}
 }
-void OpenGLWidget::resizeGL(int w,int h)
+void OpenGLWidget::resizeGL(int w, int h)
 {
 }
 
 //Draw Left Part
-void OpenGLWidget::Mini_Map()	
-{					
+void OpenGLWidget::Mini_Map()
+{
 	glBegin(GL_LINES);
 
-		float viewerPosX = MazeWidget::maze->viewer_posn[Maze::X];
-		float viewerPosY = MazeWidget::maze->viewer_posn[Maze::Y];
-		float viewerPosZ = MazeWidget::maze->viewer_posn[Maze::Z];
+	float viewerPosX = MazeWidget::maze->viewer_posn[Maze::X];
+	float viewerPosY = MazeWidget::maze->viewer_posn[Maze::Y];
+	float viewerPosZ = MazeWidget::maze->viewer_posn[Maze::Z];
 
-		for(int i = 0 ; i < (int)MazeWidget::maze->num_edges; i++)
+	for (int i = 0; i < (int)MazeWidget::maze->num_edges; i++)
+	{
+		float edgeStartX = MazeWidget::maze->edges[i]->endpoints[Edge::START]->posn[Vertex::X];
+		float edgeStartY = MazeWidget::maze->edges[i]->endpoints[Edge::START]->posn[Vertex::Y];
+		float edgeEndX = MazeWidget::maze->edges[i]->endpoints[Edge::END]->posn[Vertex::X];
+		float edgeEndY = MazeWidget::maze->edges[i]->endpoints[Edge::END]->posn[Vertex::Y];
+
+		glColor3f(MazeWidget::maze->edges[i]->color[0], MazeWidget::maze->edges[i]->color[1], MazeWidget::maze->edges[i]->color[2]);
+		if (MazeWidget::maze->edges[i]->opaque)
 		{
-			float edgeStartX = MazeWidget::maze->edges[i]->endpoints[Edge::START]->posn[Vertex::X];
-			float edgeStartY = MazeWidget::maze->edges[i]->endpoints[Edge::START]->posn[Vertex::Y];
-			float edgeEndX = MazeWidget::maze->edges[i]->endpoints[Edge::END]->posn[Vertex::X];
-			float edgeEndY = MazeWidget::maze->edges[i]->endpoints[Edge::END]->posn[Vertex::Y];
-
-			glColor3f(MazeWidget::maze->edges[i]->color[0] , MazeWidget::maze->edges[i]->color[1], MazeWidget::maze->edges[i]->color[2]);
-			if(MazeWidget::maze->edges[i]->opaque)
-			{
-				glVertex2f(edgeStartX, edgeStartY);
-				glVertex2f(edgeEndX, edgeEndY);
-			}
+			glVertex2f(edgeStartX, edgeStartY);
+			glVertex2f(edgeEndX, edgeEndY);
 		}
+	}
 
-		//draw frustum
-		float len = 0.1;
-		glColor3f(1, 1, 1);
-		glVertex2f(viewerPosX, viewerPosY);
-		glVertex2f(viewerPosX + (MazeWidget::maze->max_xp) * len * cos(degree_change(MazeWidget::maze->viewer_dir - MazeWidget::maze->viewer_fov/2)) ,
-			viewerPosY + (MazeWidget::maze->max_yp) * len * sin(degree_change(MazeWidget::maze->viewer_dir - MazeWidget::maze->viewer_fov/2)));
+	//draw frustum
+	float len = 0.1;
+	glColor3f(1, 1, 1);
+	glVertex2f(viewerPosX, viewerPosY);
+	glVertex2f(viewerPosX + (MazeWidget::maze->max_xp) * len * cos(degree_change(MazeWidget::maze->viewer_dir - MazeWidget::maze->viewer_fov / 2)),
+		viewerPosY + (MazeWidget::maze->max_yp) * len * sin(degree_change(MazeWidget::maze->viewer_dir - MazeWidget::maze->viewer_fov / 2)));
 
-		glVertex2f(viewerPosX, viewerPosY);
-		glVertex2f(viewerPosX + (MazeWidget::maze->max_xp) * len * cos(degree_change(MazeWidget::maze->viewer_dir + MazeWidget::maze->viewer_fov/2)) ,
-			viewerPosY + (MazeWidget::maze->max_yp) * len *  sin(degree_change(MazeWidget::maze->viewer_dir + MazeWidget::maze->viewer_fov/2)));
+	glVertex2f(viewerPosX, viewerPosY);
+	glVertex2f(viewerPosX + (MazeWidget::maze->max_xp) * len * cos(degree_change(MazeWidget::maze->viewer_dir + MazeWidget::maze->viewer_fov / 2)),
+		viewerPosY + (MazeWidget::maze->max_yp) * len *  sin(degree_change(MazeWidget::maze->viewer_dir + MazeWidget::maze->viewer_fov / 2)));
 	glEnd();
 }
 
@@ -120,22 +120,22 @@ void OpenGLWidget::Map_3D()
 {
 	glLoadIdentity();
 	// 畫右邊區塊的所有東西
-	
+
 
 
 	/*若有興趣的話, 可以為地板或迷宮上貼圖, 此項目不影響評分*/
 	glBindTexture(GL_TEXTURE_2D, sky_ID);
-	
+
 	// 畫貼圖 & 算 UV
-	
+
 	glDisable(GL_TEXTURE_2D);
 }
-void OpenGLWidget::loadTexture2D(QString str,GLuint &textureID)
+void OpenGLWidget::loadTexture2D(QString str, GLuint &textureID)
 {
 	glGenTextures(1, &textureID);
 	glBindTexture(GL_TEXTURE_2D, textureID);
 	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
-	
+
 	QImage img(str);
 	QImage opengl_grass = QGLWidget::convertToGLFormat(img);
 
@@ -146,5 +146,5 @@ void OpenGLWidget::loadTexture2D(QString str,GLuint &textureID)
 }
 float OpenGLWidget::degree_change(float num)
 {
-	return num /180.0f * 3.14159f;
+	return num / 180.0f * 3.14159f;
 }
